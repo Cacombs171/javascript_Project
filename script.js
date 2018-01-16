a23f9fe9bcbd1cfcc827e74f4c566bb77afdb95a
Cacombs171 <31549532+Cacombs171@users.noreply.github.com>
2018-01-16 11:59:29 -0500

    no message

---
+++ script.js
@@ -0,0 +1,49 @@
+//function showDate(){
+  //document.getElementById('datePlace').innerHTML = Date();
+//}
+
+function displayTime(){
+  var date = new Date();
+  var year= date.getYear();
+  if(year < 1000){
+    year +=1900
+  }
+  var day = date.getDay();
+  var month = date.getMonth();
+  var monthDay = date.getDate();
+  var dayArray = new Array("Sunday","Monday","Tuesday","WeWednesday","Thursday","Friday","Saturday");
+  var monthArray = new Array("August","September","October","November","December","January","Febuary","March","April","May")
+
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  if(hours == 24){
+  hours = 0;
+} else if(hours > 12){
+  hours = hours - 0;
+}
+
+if(hours < 10){
+  hours = "0" + hours;
+}
+if(minutes < 10){
+  minutes = "0" + minutes;
+}
+
+if(seconds < 10){
+  seconds = "0" + seconds;
+}
+
+var clock = document.getElementById("showDate");
+clock.textContent = "" +dayArray[day]+ " " +monthDay+ " "+monthArray[month]+ " " +year+ " | " +hours+ " : " +minutes+ " : " +seconds;
+clock.innerText = "" +dayArray[day]+ " " +monthDay+ " "+monthArray[month]+ " " +year+ " | " +hours+ " : " +minutes+ " : " +seconds;
+
+setTimout("displayTime()", 1000);
+}
+
+displayTime();
+
+
+
+//https://www.youtube.com/watch?v=HyhVjHGb19k&t=1108s
